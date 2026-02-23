758974aa6192d968cc0682536bd884afacf47783
0XSaurabhSharma <101334796+Git-SaurabhSharma@users.noreply.github.com>
2026-02-23 15:33:49 +0530

    error tracking

--- src/app/api/demo/blocking/route.ts
+++ src/app/api/demo/blocking/route.ts
@@ -7,6 +7,11 @@ export const POST = async () => {
     const response = await generateText({
         model: google('gemini-2.5-flash'),
         prompt: 'utter 4 words in chinese, but write in english, and give meaning.',
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
     });
 
     return Response.json({response})
--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@base-ui/react';
 import React, { useState } from 'react'
+import * as Sentry from "@sentry/nextjs";
 
 const DemoPage = () => {
 
@@ -23,6 +24,23 @@ const DemoPage = () => {
         })
         setLoadingBackground(false)
     }
+
+    const clientError = ()=> {
+        Sentry.captureException(new Error("Client Error: This is a error in browser."))
+    }
+
+    const apiError = async () => {
+        await fetch('api/demo/api-error', {
+            method: 'POST'
+        })
+    }
+
+    const inngestError = async ()=> {
+        Sentry.logger.info("Inngest triggered: user initiated background job.")
+        await fetch("api/demo/inngest-error", {
+            method: 'POST'
+        })
+    }
   return (
     <div className='flex gap-2 p-8'>
         <Button className='border-2 py-2 px-4 bg-amber-700 rounded-lg' onClick={handleBlocking} disabled={loadingBlock}>
@@ -30,6 +48,15 @@ const DemoPage = () => {
         </Button>
         <Button className='border-2 py-2 px-4 bg-amber-700 rounded-lg' onClick={handleBackground} disabled={loadingBackground}>
             {loadingBackground ? 'Background job...' : 'Background'}
+        </Button>
+        <Button className='border-2 py-2 px-4 bg-amber-700 rounded-lg' onClick={clientError}>
+            Client Error
+        </Button>
+        <Button className='border-2 py-2 px-4 bg-amber-700 rounded-lg' onClick={apiError}>
+            API Error
+        </Button>
+        <Button className='border-2 py-2 px-4 bg-amber-700 rounded-lg' onClick={inngestError}>
+            Background Jobs Error
         </Button>
     </div>
   )

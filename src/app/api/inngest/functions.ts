3184fece80ac02c1da711361cb5e57be83d23a0f
0XSaurabhSharma <101334796+Git-SaurabhSharma@users.noreply.github.com>
2026-02-23 12:21:57 +0530

    firecrawl ai

--- src/app/api/inngest/functions.ts
+++ src/app/api/inngest/functions.ts
@@ -1,4 +1,5 @@
 import { inngest } from "@/inngest/client"; 
+import { firecrawl } from "@/lib/firecrawl";
 import { google } from '@ai-sdk/google';
 import { generateText } from 'ai';
 
@@ -24,14 +25,42 @@ import { generateText } from 'ai';
 //     }
 // )
 
+const URL_REGEX = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
 export const getResponse = inngest.createFunction(
     { id: "get-response" },
     { event: "get/response" },
-    async ({ step }) => {
+    async ({ event, step }) => {
+
+        // prompt -> urls -> crawl url -> final_prompt -> invoke
+
+        const {prompt} = event.data as {prompt: string;}
+        
+        const urls = await step.run('extract-urls', async ()=> {
+            return prompt.match(URL_REGEX) ?? [];
+        }) as string[];
+
+        const scrapedContent = await step.run('scrape-urls', async ()=> {
+            const results = await Promise.all(
+                urls.map (async (url) => {
+                    const res = await firecrawl.scrape(
+                        url,
+                        { formats: ['markdown']}
+                    );
+                    return res.markdown ?? null;
+                })
+            );
+            return results.filter(Boolean).join('\n\n')
+        })
+
+        const finalPrompt = scrapedContent
+        ? `${prompt}\n\nScraped Content:\n\n${scrapedContent}`
+        : prompt;
+
         return await step.run('generate-text', async () => {
             const result = await generateText({
                 model: google('gemini-2.5-flash'),
-                prompt: 'how to not take ourself seriouly ? in one sentence.',
+                prompt: finalPrompt,
             });
             return { response: result };
         });
